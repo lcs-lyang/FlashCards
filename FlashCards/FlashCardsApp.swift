d2d1860c79a4ffcbc51c2c43aeeaec7f8fc65526
Lillian Yang <lyang@lcs.on.ca>
2021-12-10 11:12:03 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Lillian Yang on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
